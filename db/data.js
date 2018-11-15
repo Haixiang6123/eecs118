906aa0588e2027a202137abbd8121f354596be8e
Haixiang <haixiang6123@gmail.com>
2018-11-14 18:57:44 -0800

    Init projects

---
+++ db/data.js
@@ -0,0 +1,64 @@
+const db = {
+    'Group A': {
+        category: 'Group A',
+        members: [{
+                name: 'Silver Zhang',
+                url: '#'
+            },
+            {
+                name: 'Aakif Hussaini',
+                url: '#'
+            },
+            {
+                name: 'Derrell Record',
+                url: '#'
+            },
+            {
+                name: 'Benjamin Ng',
+                url: '#'
+            }
+        ],
+        images: [
+            'assets/A/Circle and Triangle 14.JPG',
+            'assets/A/Line and Triangle 1.JPG'
+        ]
+    },
+    'Group B': {
+        category: 'Group B',
+        members: [{
+                name: 'Zunwen Li',
+                url: '#'
+            },
+            {
+                name: 'Hongyi Li',
+                url: '#'
+            },
+            {
+                name: 'Zhifang Zeng',
+                url: '#'
+            }
+        ],
+        images: [
+            'assets/B/Circle and Triangle 15.JPG'
+        ]
+    },
+    'Group C': {
+        category: 'Group C',
+        members: [{
+                name: 'Steven Long',
+                url: '#'
+            },
+            {
+                name: 'Diego Torres',
+                url: '#'
+            },
+            {
+                name: 'Arian Reyes',
+                url: '#'
+            }
+        ],
+        images: [
+            'assets/C/Circle and Triangle 16.JPG',
+        ]
+    },
+}
---
+++ scripts/main.js
@@ -0,0 +1,51 @@
+! function () {
+    renderGroup('Group A')
+}()
+
+function selectGroup(event) {
+    event.preventDefault();
+
+    let $target = $(event.target)
+    let $dropdownTrigger = $('#dropdown-trigger')
+    let groupId = $target.text()
+
+    // Update trigger text
+    $dropdownTrigger.text(groupId)
+
+    // Render a group to front page
+    renderGroup(groupId)
+}
+
+function renderGroup(groupId) {
+    let groupData = db[groupId]
+    let $problemImages = $('#problem-images')
+    let $members = $('#members')
+    let $groupTitle = $('#group-title')
+
+    $problemImages.empty()
+    $members.empty()
+
+    $groupTitle.text(groupId)
+
+    groupData.images.forEach(imageSrc => {
+        $problemImages.append(`<img class="d-block w-100" src="${imageSrc}">`)
+    });
+
+    groupData.members.forEach(memberData => {
+        $members.append(`<p><a href="${memberData.url}">${memberData.name}</a></p>`)
+    })
+
+    setTimeout(() => {
+        animate()
+    }, 10);
+}
+
+function animate() {
+    $('#problem-images img').each(function () {
+        $(this).addClass('active')
+    })
+
+    $('#members p').each(function () {
+        $(this).addClass('active')
+    })
+}
